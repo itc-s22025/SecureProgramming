3329f06196f301fe1ec5ce4a94c59546e3fe0dd1
s22025 <s22025@std.it-college.ac.jp>
2024-07-18 16:34:50 +0900

    とりあえず43コミット

---
+++ 43/43-001.php
@@ -0,0 +1,8 @@
+<?php
+	session_start();
+	// ログインチェック
+?>
+<body>
+検索キーワード：<?php echo $_GET['keyword']; ?> <br>
+以下略
+</body>
---
+++ 43/43-012.php
@@ -0,0 +1,5 @@
+<head><script>
+function init(a) {}	//ダミー
+</script><head>
+<body onload="init('<?php echo htmlspecialchars($_GET['name'], ENT_QUOTES) ?>')">
+</body>
